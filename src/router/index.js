c5b75fd6658865504f3ed5bf65864e7a6a7af55d
尹玉慧 <yinyuhui21@sina.com>
2018-05-02 13:33:47 +0800

    '头部脚部面包屑封装为组件'

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,13 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import GoodsList from '@/views/GoodsList.vue'
 
 Vue.use(Router)
 
 export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
-  ]
+    routes: [{
+        path: '/',
+        name: 'GoodsList',
+        component: GoodsList
+    }]
 })
---
+++ src/views/Bread.vue
@@ -0,0 +1,11 @@
+<template>
+   <div class="nav-breadcrumb-wrap">
+        <div class="container">
+            <nav class="nav-breadcrumb">
+                <a href="/">Home</a>
+                <slot></slot>
+                <!-- <span>Goods</span> -->
+            </nav>
+        </div>
+    </div>
+</template>
