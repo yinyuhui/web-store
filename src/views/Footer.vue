24138ba86aa9a2f03bb2d4eb0ee50941f26c3fc5
尹玉慧 <yinyuhui21@sina.com>
2018-05-05 15:52:08 +0800

    修改一点样式

--- src/views/Footer.vue
+++ src/views/Footer.vue
@@ -1,16 +1,23 @@
+<style>
+.footer__secondary__nav{
+  margin-top: 0;
+}
+</style>
+
+
 <template>
   <footer class="footer">
         <div class="footer__wrap">
           <div class="footer__secondary">
             <div class="footer__inner">
-              <div class="footer__region">
-                <span>地区</span>
+              <!-- <div class="footer__region"> -->
+                <!-- <span>地区</span>
                 <select class="footer__region__select">
                     <option value="en-US">USA</option>
                     <option value="zh-CN">China</option>
                     <option value="in">India</option>
-                </select>
-              </div>
+                </select> -->
+              <!-- </div> -->
               <div class="footer__secondary__nav">
                     <span>Copyright © 2018 Yin Yuhui All Rights Reserved.</span>
                     <a href="https://github.com/yinyuhui/web-store">
